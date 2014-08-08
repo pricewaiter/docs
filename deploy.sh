701eb66d673b59e9bae772cab8fb631384294b0e
Grant Regimbal <Regimbal.grant@gmail.com>
2014-08-08 12:31:51 -0700

    try without --deployment

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,7 @@
 #
 # Script for jenkins to run to build jekyll & cap deploy
 
-bundle install --path vendor/bundle --deployment
-
+bundle install --path vendor/bundle
 bundle exec jekyll build
 
 bundle exec cap deploy 
